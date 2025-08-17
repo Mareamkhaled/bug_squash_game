b86d99364fe9733d81b2a991d0ac32519e535469
maream khaled ahmed <mareamkmousbah@gmail.com>
2025-08-17 09:16:12 +0300

    Refactor bug creation and scoring logic; enhance score display on tap

--- lib/bug_squash_game.dart
+++ lib/bug_squash_game.dart
@@ -1,14 +1,15 @@
 import 'dart:async';
 import 'dart:math';
-import 'dart:ui';
 
+import 'package:flutter/material.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
 import 'bug.dart';
 
 class BugSquashGame extends FlameGame{
-
+late TextComponent _scoreComponent;
+  int _score = 0;
  late Timer _interval;
 
   BugSquashGame() {
@@ -26,12 +27,18 @@ class BugSquashGame extends FlameGame{
   return const Color.fromARGB(255, 111, 167, 163); // 
   }
 
-  // @override
-  // FutureOr<void> onLoad()async {
-  //  final bugComponent = _createBug();
-   
-  //   return super.onLoad();
-  // }
+   @override
+  FutureOr<void> onLoad() {
+    _scoreComponent = TextComponent(
+      text: "$_score",
+      textRenderer: TextPaint(
+        style:const TextStyle(color: Colors.black, fontSize: 24.0),
+      ),
+    );
+    _scoreComponent.anchor = Anchor.center;
+    _scoreComponent.position = Vector2(size.x / 2, 100);
+    add(_scoreComponent);
+  }
 
  void _createBug() {
     final bugComponent = Bug();
@@ -41,6 +48,7 @@ class BugSquashGame extends FlameGame{
     bugComponent.position = Vector2(0, randomYPosition);
     bugComponent.angle = pi / 2;
     bugComponent.onTap = () {
+      _scoreComponent.text = "${_score += 1}";
       Future.delayed(const Duration(milliseconds: 500)).then(
         (value) {
           if (!bugComponent.isRemoved) {
