0684298e002f56798193a95c28ed9b0a22f0be13
maream khaled ahmed <mareamkmousbah@gmail.com>
2025-08-17 08:09:08 +0300

    +display bug on the screen
    +Refactor BugSquashGame class and update widget test for GameWidget integration

--- lib/bug_squash_game.dart
+++ lib/bug_squash_game.dart
@@ -1,5 +1,26 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
 class BugSquashGame extends FlameGame{
+  @override
+  Color backgroundColor() {
+  return const Color(0xFFB2DFDB); // 
+  }
+
+  @override
+  FutureOr<void> onLoad()async {
+   final bugSprite = await Sprite.load('bug.png');
+   final bugComponent = SpriteComponent(
+      sprite: bugSprite,
+      anchor: Anchor.center, 
+      size: Vector2(100, 150), 
+      position: Vector2(100, 300)
+    );
+    add(bugComponent);
+    return super.onLoad();
+  }
   
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:bug_squash_game/bug_squash_game.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +14,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(BugSquashGame());
+    await tester.pumpWidget(GameWidget(game: BugSquashGame()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
