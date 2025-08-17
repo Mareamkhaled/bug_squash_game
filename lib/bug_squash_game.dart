4cfb34f6025822c7cacda847f5039cbf01bcdf6d
maream khaled ahmed <mareamkmousbah@gmail.com>
2025-08-17 08:57:21 +0300

    Add tap handling for bug component to remove it after a delay

--- lib/bug_squash_game.dart
+++ lib/bug_squash_game.dart
@@ -18,6 +18,16 @@ class BugSquashGame extends FlameGame{
     bugComponent.position = Vector2(size.x / 2, size.y / 2);
     // print('Bug position: ${bugComponent.position}');
     bugComponent.anchor = Anchor.center;  
+    bugComponent.onTap = (){
+       Future.delayed(const Duration(milliseconds: 500)).then(
+        (value) {
+          if (!bugComponent.isRemoved) {
+            remove(bugComponent);
+          }
+        },
+      );
+      
+    };
     add(bugComponent);
     return super.onLoad();
   }
